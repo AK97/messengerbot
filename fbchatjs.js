c5fdc122ac939105c874c31a67a64ff3f25d2ccf
tgog <tgog@umich.edu>
2016-11-05 17:26:42 -0400

    status works

--- fbchatjs.js
+++ fbchatjs.js
@@ -13,16 +13,17 @@ login({email: "clayytonbhig@gmail.com", password: "naisubhig"}, function callbac
     api.sendMessage("Enter B H I G", group);
     var me = '100000921889753'; //roon's user ID
 	var tracking_data = {};
+	var thread_info = {};
 	var deeb = '100003952090241'; //aaron's user ID
 
     api.getThreadInfo(group, function(err, info) {
     	if (err) return console.error(err);
-
-    	console.log("Here is info, \n", info);
+    	thread_info = info;
+    	console.log("Here is info, \n", thread_info);
     	console.log("here is the participantIDs variable \n", info.participantIDs);
 
-    	for (var x in info.participantIDs) {
-    		var y = info.participantIDs[x];
+    	for (var x in thread_info.participantIDs) {
+    		var y = thread_info.participantIDs[x];
     		tracking_data[y] = 0;
     	}
 	    console.log("tracking_data: ");
@@ -42,23 +43,36 @@ login({email: "clayytonbhig@gmail.com", password: "naisubhig"}, function callbac
 		        				return stopListening();
 		        			}
 		        			if(event.body === '/status') {
-					        	var output = "";
-					        	for (var x in tracking_data) {
-					        		if (x in info.nicknames) {
-					        			var temp = info.nicknames[x];
-					        			output = output + temp + ":";
-					        			for (var x = 0; x < 30 - temp.length; x++) {
-					        				output += " ";
-					        			}
-					        		} else {
-					        			output = output + x + ":";
-					        			for (var x = 0; x < 30 - x.length; x++) {
-					        				output += " ";
-					        			}
-					        		}
-					        		output = output + tracking_data[x] + '\n';
-					        	}
-					        	api.sendMessage(output, group);
+		        				api.getThreadInfo(group, function(err, info) {
+		        					if (err) return console.error(err);
+			        				thread_info = info;
+						        	var output = tracking_data;
+						        	for (var x in thread_info.nicknames) {
+						        		if (x in output) {
+						        			output[thread_info.nicknames[x]] = output[x];
+											delete output[x];
+						        		}
+						        	}
+						        	api.sendMessage(JSON.stringify(output, null, 4), group);
+
+						        	// for (var x in tracking_data) {
+						        	// 	if (x in thread_info.nicknames) {
+						        	// 		var temp = thread_info.nicknames[x];
+						        	// 		output = output + temp + ":";
+						        	// 		for (var x = 0; x < 30 - temp.length; x++) {
+						        	// 			output += " ";
+						        	// 		}
+						        	// 	} else {
+						        	// 		output = output + x + ":";
+						        	// 		for (var x = 0; x < 30 - x.length; x++) {
+						        	// 			output += " ";
+						        	// 		}
+						        	// 	}
+						        	// 	output = output + tracking_data[x] + '\n';
+						        	// }
+						        	// api.sendMessage(output, group);		        					
+		        				});
+
 
 					        	// str = JSON.stringify(info.nicknames, null, 4); 
 					   	     //    api.sendMessage(str, group);
@@ -123,7 +137,7 @@ login({email: "clayytonbhig@gmail.com", password: "naisubhig"}, function callbac
 		        	break;
 
 		        case "read_receipt":
-		        	if (Math.random() > .8)
+		        	if (Math.random() > .99)
 		        		api.sendMessage("Well, Tarnum?",group);
 		        	break;
 		    	}
