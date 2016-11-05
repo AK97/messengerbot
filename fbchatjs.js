030d4adcb1be1d0921bb5be57b4b31b4f3b38b6c
tgog <tgog@umich.edu>
2016-11-05 01:36:55 -0400

    hehe xd

--- fbchatjs.js
+++ fbchatjs.js
@@ -1,7 +1,7 @@
 var login = require("facebook-chat-api");
 const fs = require("fs"); 
 
-login({email: "tarun.gogineni@gmail.com", password: "4454vanhoosen"}, function callback (err, api) {
+login({email: "", password: ""}, function callback (err, api) {
     if(err) return console.error(err);
 
     api.setOptions({
