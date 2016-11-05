edb526f4692748b8843f389ec219a24c457eee85
tgog <tgog@umich.edu>
2016-11-05 19:02:33 -0400

    status works finally?

--- fbchatjs.js
+++ fbchatjs.js
@@ -1,7 +1,8 @@
 var login = require("facebook-chat-api");
 const fs = require("fs");
 var cleverbot = require('cleverbot.io');
-
+var jsonfile = require('jsonfile');
+var extend = require('util')._extend;
 
 
 var bot = new cleverbot('TYHRwNcZFicTF4xI','rKarZL4vSevwLDnjLXnGK7MRkBwud1W1');
@@ -18,21 +19,30 @@ login({email: "clayytonbhig@gmail.com", password: "naisubhig"}, function callbac
 
     var group = '1144974435591141'; //test chat pls ignore
     api.sendMessage("Enter B H I G", group);
-    var me = '100000921889753'; //roon's user ID
+    var roon = '100000921889753'; //roon's user ID
 	var tracking_data = {};
 	var thread_info = {};
 	var deeb = '100003952090241'; //aaron's user ID
+	var file_exists = true;
+	try {
+		tracking_data = jsonfile.readFileSync('tracking_data.json');
+	} catch(err) {
+		file_exists = false;
+	}
+	console.log(typeof big_data);
 
     api.getThreadInfo(group, function(err, info) {
     	if (err) return console.error(err);
     	thread_info = info;
     	console.log("Here is info, \n", thread_info);
     	console.log("here is the participantIDs variable \n", info.participantIDs);
-
-    	for (var x in thread_info.participantIDs) {
-    		var y = thread_info.participantIDs[x];
-    		tracking_data[y] = 0;
+    	if (!file_exists) {
+	    	for (var x in thread_info.participantIDs) {
+	    		var y = thread_info.participantIDs[x];
+	    		tracking_data[y] = 0;
+	    	}
     	}
+
 	    console.log("tracking_data: ");
 	    var str = JSON.stringify(tracking_data, null, 4); // (Optional) beautiful indented output.
 		console.log(str);
@@ -47,45 +57,31 @@ login({email: "clayytonbhig@gmail.com", password: "naisubhig"}, function callbac
 	            		if (input) {
 	            			if(input === '/stopthemadness') {
 		        				api.sendMessage("fuk the frik off", group);
+		        				var file = 'tracking_data.json'
+								 
+								jsonfile.writeFile(file, tracking_data, function (err) {
+									if (err) console.error(err);
+								})
 		        				return stopListening();
 		        			}
-		        			if(event.body === '/status') {
+		        			if(input === '/status') {
 		        				api.getThreadInfo(group, function(err, info) {
 		        					if (err) return console.error(err);
 			        				thread_info = info;
-						        	var output = tracking_data;
+						        	var output = extend({}, tracking_data);
+						        	console.log(thread_info.nicknames);
 						        	for (var x in thread_info.nicknames) {
 						        		if (x in output) {
 						        			output[thread_info.nicknames[x]] = output[x];
 											delete output[x];
 						        		}
 						        	}
-						        	api.sendMessage(JSON.stringify(output, null, 4), group);
-
-						        	// for (var x in tracking_data) {
-						        	// 	if (x in thread_info.nicknames) {
-						        	// 		var temp = thread_info.nicknames[x];
-						        	// 		output = output + temp + ":";
-						        	// 		for (var x = 0; x < 30 - temp.length; x++) {
-						        	// 			output += " ";
-						        	// 		}
-						        	// 	} else {
-						        	// 		output = output + x + ":";
-						        	// 		for (var x = 0; x < 30 - x.length; x++) {
-						        	// 			output += " ";
-						        	// 		}
-						        	// 	}
-						        	// 	output = output + tracking_data[x] + '\n';
-						        	// }
-						        	// api.sendMessage(output, group);		        					
+						        	api.sendMessage(JSON.stringify(output, null, 4), group);	        					
 		        				});
-
-
-					        	// str = JSON.stringify(info.nicknames, null, 4); 
-					   	     //    api.sendMessage(str, group);
-					        	// str = JSON.stringify(tracking_data, null, 4);
-					        	// api.sendMessage(str, group);
-					        }		
+					        }
+					        if(input === '/statustest') {
+					        	api.sendMessage(JSON.stringify(tracking_data, null, 4), group);	
+					        }	
 		        			if(input === '/kukup') {
 		        				api.sendMessage("HO HO HO", group);
 		        				api.changeNickname("Bhuge Dumbass", group, deeb);
@@ -102,9 +98,12 @@ login({email: "clayytonbhig@gmail.com", password: "naisubhig"}, function callbac
 		        			}
 		        			if(input === '/surendrekt') {
 		        				api.sendMessage("Surendrekt", group);
-		        				api.removeUserFromGroup(me, group, function callback(err){
+		        				api.removeUserFromGroup(roon, group, function callback(err){
 				        			if(err) return console.error(err);
 				    			});
+				    			setTimeout(api.addUserToGroup(roon, group, function(err){
+				    				if (err) return console.error(err);
+				    			}), 5000);
 		        			}
 		        			if (input.indexOf("/8ball")==0 || input.indexOf("🎱")==0) {
 	                			var eightball = 
@@ -152,7 +151,7 @@ login({email: "clayytonbhig@gmail.com", password: "naisubhig"}, function callbac
 		        	break;
 
 		        case "read_receipt":
-		        	if (Math.random() > .99)
+		        	if (Math.random() > .99 && event.reader == roon)
 		        		api.sendMessage("Well, Tarnum?",group);
 		        	break;
 		    	}
