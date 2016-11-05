c3a90eeed224898b33ef619bc174bd8a1f2ec61d
tgog <tgog@umich.edu>
2016-11-05 16:27:09 -0400

    sort of fixed /status

--- fbchatjs.js
+++ fbchatjs.js
@@ -1,5 +1,6 @@
 var login = require("facebook-chat-api");
 const fs = require("fs");
+const util = require("util");
 
 login({email: "clayytonbhig@gmail.com", password: "naisubhig"}, function callback (err, api) {
     if(err) return console.error(err);
@@ -10,47 +11,77 @@ login({email: "clayytonbhig@gmail.com", password: "naisubhig"}, function callbac
     });
 
     var group = '1144974435591141';
-    api.sendMessage("tracking bot is tracking", group);
     var me = '100000921889753';
-	var data = {};
-	var threadInfo;
+    api.sendMessage("tracking bot is tracking", group);
+
+	var tracking_data = {};
 	var deeb = '100003952090241';
     api.getThreadInfo(group, function(err, info) {
-    	console.log(info);
-    	threadInfo = info;
-    	for (var x in info.participantIDs) {
-    		data.x = 0;
-    	}
-    });
+    	if (err) return console.error(err);
 
-    var stopListening = api.listen(function(err, message) {
-        if (err) return console.error(err);
-        if (message.threadID == group) {
-	        if(message.body === '/stopthemadness') {
-	        	api.sendMessage("fuk the frik off", group);
-	        	return stopListening();
-	        }
-	        if(message.body === '/status') {
-	        	api.sendMessage("still working", group);
-	   	        api.sendMessage(str(threadInfo.nicknames), group);
-	        	api.sendMessage(str(data), group);
-	        }
-	        if(message.body === '/kukup') {
-	        	api.sendMessage("HO HO HO", group);
-	        	api.changeNickname("Bhuge Dumbass", group, deeb);
-	        }
-	        if(message.body === '/dab') {
-	        	api.sendMessage({attachment: fs.createReadStream('dab.png')}, group);
-	        	api.changeNickname("Bhuge Dumbass", group, deeb);
-	        }
-	        if(message.body === '/gloriousdawn') {
-	        	api.sendMessage("HO HO HO", group);
-	        	api.changeGroupImage(fs.createReadStream("./sombrerokul.jpg"), group, function callback(err) {
-			        if(err) return console.error(err);
-			    });
-	        }
-	        var user = message.userID;
-	        data.user++;
-        } 
-    });
+    	console.log("Here is info, \n", info);
+    	console.log("here is the participantIDs variable \n", info.participantIDs);
+
+    	for (var x in info.participantIDs) {
+    		var y = info.participantIDs[x];
+    		tracking_data[y] = 0;
+    	}
+	    console.log("tracking_data: ");
+	    var str = JSON.stringify(tracking_data, null, 4); // (Optional) beautiful indented output.
+		console.log(str);
+
+	    var stopListening = api.listen(function(err, message) {
+	    	console.log(message);
+	        if (err) return console.error(err);
+	        if (message.threadID == group) {
+		        if(message.body === '/stopthemadness') {
+		        	api.sendMessage("fuk the frik off", group);
+		        	return stopListening();
+		        }
+		        if(message.body === '/status') {
+		        	var output = "";
+		        	for (var x in tracking_data) {
+		        		if (x in info.nicknames) {
+		        			var temp = info.nicknames[x];
+		        			output = output + temp + ":";
+		        			for (var x = 0; x < 30 - temp.length; x++) {
+		        				output += " ";
+		        			}
+		        		} else {
+		        			output = output + x + ":";
+		        			for (var x = 0; x < 30 - x.length; x++) {
+		        				output += " ";
+		        			}
+		        		}
+		        		output = output + tracking_data[x] + '\n';
+		        	}
+		        	api.sendMessage(output, group);
+
+		        	// str = JSON.stringify(info.nicknames, null, 4); 
+		   	     //    api.sendMessage(str, group);
+		        	// str = JSON.stringify(tracking_data, null, 4);
+		        	// api.sendMessage(str, group);
+		        }
+		        if(message.body === '/kukup') {
+		        	api.sendMessage("HO HO HO", group);
+		        	api.changeNickname("Bhuge Dumbass", group, deeb);
+		        }
+		        if(message.body === '/dab') {
+		        	api.sendMessage({attachment: fs.createReadStream('dab.png')}, group);
+		        	api.changeNickname("Bhuge Dumbass", group, deeb);
+		        }
+		        if(message.body === '/gloriousdawn') {
+		        	api.sendMessage("HO HO HO", group);
+		        	api.changeGroupImage(fs.createReadStream("./sombrerokul.jpg"), group, function callback(err) {
+				        if(err) return console.error(err);
+				    });
+		        }
+		        if (message.senderID) {
+			        tracking_data[message.senderID]++;
+			        str = JSON.stringify(tracking_data, null, 4); // (Optional) beautiful indented output.
+					console.log(str);		        	
+		        }
+	        } 
+	    });
+    });    
 });
