d5a7ba98b82c8f6a46437e1cee425d2acc6cddd0
YordleLover97 <pxchoudhury@gmail.com>
2016-11-09 15:51:28 -0500

    All non data related commands now ported over to functions.js
    Also added /XD.
    
    Known bugs:
    -Rps function not properly hooked up yet

--- fbchatjs.js
+++ fbchatjs.js
@@ -1,13 +1,8 @@
 var login = require("facebook-chat-api");
 const fs = require("fs");
-var cleverbot = require('cleverbot.io');
 var jsonfile = require('jsonfile');
 var extend = require('util')._extend;
 var functions = require('./functions');
-
-var bot = new cleverbot('TYHRwNcZFicTF4xI','rKarZL4vSevwLDnjLXnGK7MRkBwud1W1');
-bot.setNick("clayton");
-bot.create(function(err,Clayton){});
 
 login({email: "kenbhone@gmail.com", password: "naisubhig"}, function callback (err, api) {
     if(err) return console.error(err);
@@ -19,49 +14,20 @@ login({email: "kenbhone@gmail.com", password: "naisubhig"}, function callback (e
 
     var group = '1007807106011631'; //real chat
     var roon = '100000921889753'; //roon's user ID
-    var pio = '100006115174010'; //pio's user ID
+    var pio = '100006115174010'; //pio's user ID    
+    var aaron = '100003952090241'; //aaron's user ID
+    var deeb = '1683495739'; //deeb's user ID	
+
+	var testChat = '1144974435591141'; // test chat
+    group = testChat;	
+	deeb = aaron;
+
+	//data stuff
 	var tracking_data = {};
 	var usageData = {};
 	var threadInfo = {};
-	var deeb = '1683495739'; //deeb's user ID
 
-	var testChat = '1144974435591141'; // test chat
-    group = testChat;
-	var aaron = '100003952090241'; //aaron's user ID
-	deeb = aaron;
-
-
-	// api.sendMessage("Enter B H I G", group);
-
-	// rps stuff
-	var rpsCountdown;
-	var gameInProgress = false; //stores whether there's an active rock-paper-scissors game
-	var playerHand = ""; //player choice for rps
-	var botHand = ""; //bot choice for rps
-
-	var eightball = 
-		[
-		"It is certain",
-		"It is decidedly so",
-		"Without a doubt", 
-		"Yes, definitely",
-		"Yes, definitely",
-		"You may rely on it",
-		"As I see it, yes",
-		"Most likely",
-		"Outlook good",
-		"Yes",
-		"Signs point to yes",
-		"Reply hazy try again",
-		"Ask again later",
-		"Better not tell you now",
-		"Cannot predict now",
-		"Concentrate and ask again",
-		"Don't count on it",
-		"My reply is no",
-		"My sources say no",
-		"Outlook not so good",
-		"Very doubtful"];
+	// api.sendMessage("Enter B H I G", group);	
 
     api.getThreadInfo(group, function(err, info) {
     	if (err) return console.error(err);
@@ -100,7 +66,7 @@ login({email: "kenbhone@gmail.com", password: "naisubhig"}, function callback (e
         				});
         				break;
         			case '/kukup':	        			
-        				functions.kukup(api, group, deeb);
+        				functions.kukUp(api, group, deeb);
         				break;
         			case '/dab':
         				functions.dab(api, group);
@@ -114,6 +80,9 @@ login({email: "kenbhone@gmail.com", password: "naisubhig"}, function callback (e
 		    		case '/hegg':
 	    				functions.hegg(api, group);
 		    			break;
+		    		case '/XD':
+        				functions.ecksDee(api, group);
+    				break;
 	    			case '/compliment':
 	    				functions.compliment(api, group, event)
 	    				break;
@@ -128,92 +97,24 @@ login({email: "kenbhone@gmail.com", password: "naisubhig"}, function callback (e
 	    				break;
 	    			case '/hoot':
 	    				functions.hoot(api, group, threadInfo)
-	    				break;
-		    		default:        			
-		    			if(input === '/rps' || input === "/jkp" || gameInProgress) {
-		    				if (!gameInProgress) {	//makes sure there isn't an ongoing game before starting a new one	        						        					
-		    					api.sendMessage("Saisho wa guu!", group);
-		    					setTimeout(function() {
-		    						api.sendMessage("Janken pon!", group);
-		    					}, 1500);
-		    					gameInProgress = true; //flag to make sure new games can't start until this one is done
-		    					botHand = ["✊","✋","✌"][Math.floor(Math.random()*3)]; //randomize bot choice	
-		    					//start a countdown to end the game if the player doesn't make a choice	in time       							
-		    						rpsCountdown = setTimeout(function() { 	        					
-			    						console.log("Game timed out"); 		        						        					
-			    						gameInProgress = false;
-			    					}, 10000);
-			    				return;
-		    				}
-		    				//set playerHand as per valid user input
-		    					if(input === '/rock' || input === "/guu" || input === "✊") 
-		    						playerHand = "✊";       								        						        					
-		    					if(input === '/paper' || input === "/paa" || input === "✋") 
-		    						playerHand = "✋";  			        								        						        					
-		    					if(input === '/scissors' || input === "/choki" || input === "✌") 
-		    						playerHand = "✌";
-		    					//game logic		        								        								        						
-		    					if (playerHand === "✊" || playerHand === "✋" || playerHand === "✌" ) {
-		    						var winner = "It's a tie!";
-		    						if (playerHand === "✊") {
-		    							if (botHand === "✋")
-		    								winner = "bot";
-		    							if (botHand === "✌")
-		    								winner = "player";
-		    						}
-		    						if (playerHand === "✋") {		        							
-		    							if (botHand === "✌")
-		    								winner = "bot";
-		    							if (botHand === "✊")
-		    								winner = "player";
-		    						}
-		    						if (playerHand === "✌") {
-		    							if (botHand === "✊")
-		    								winner = "bot";
-		    							if (botHand === "✋")
-		    								winner = "player";
-		    						}
-		    						if(winner === "bot")
-		    							winner = "Looks like I win! Naisu."
-		    						if(winner === "player")
-		    							winner = "Looks like you win! Naisu."
-		    						clearTimeout(rpsCountdown); //cancel game timeout if game resolves successfully
-		    						gameInProgress = false;	
-		    						playerHand = "";	
-		    						api.sendMessage(botHand, group, function() {
-		    							api.sendMessage(winner, group);	
-		    						}); 						
-		    					}					        					   		        				    				  					        				
-		    			}
-		    			if (input.indexOf("/8ball")==0 || input.indexOf("🎱")==0) {
-		            		if (event.senderID == deeb) {
-		            			api.sendMessage("Debarshi, you bitch", group);
-		            		} else {
-		               			api.sendMessage(eightball[Math.floor(Math.random()*20)], group);
-		            		}
-		    			}
-		    			if(input.indexOf("/talk")==0){
-							var talking = input.replace("/talk ","");
-							bot.ask(talking,function(err,response){
-								if (err) return console.error(err);
-								api.sendMessage(response,group);
-							});
-						}
-						if(input.search("Clayyton") >= 0 || input.search("clayyton") >= 0) {
-							bot.ask(input,function(err,response){
-								if (err) return console.error(err);
-								api.sendMessage(response,group);
-							});
-						}
-	    		}
-	    		if (Math.random() > .99) {
-		        	bot.ask(input, function(err, response) {
-						if (err) return console.error(err);
-		        		api.sendMessage(response, group);
-		        	});
-		        }
+	    				break;       			
+		    		case '/rps': case '/jkp':
+		    			functions.jankenPon(api, group, input);			    				  					        				
+		    			break;
+		    		default:  
+		    			if (input.indexOf("/8ball")==0 || input.indexOf("🎱")==0) 
+		            		functions.eightball(api, group, event, deeb)		    			
+		    			else if(input.indexOf("/talk")==0) 
+		    				functions.talk(api, group, input.replace("/talk ",""));						
+						else if(input.search("Clayyton") >= 0 || input.search("clayyton") >= 0) 
+							functions.talk(api, group, input);						
+	    		}	    
+
+	    		if (Math.random() > .99) 
+		        	functions.talk(api, group, input);		        
 				if (event.senderID) tracking_data[event.senderID]++;		        	
 		    }
+
 	        else if (event.type == "read_receipt"){
 	        	if (Math.random() > .80 && event.reader == roon)
 	        		api.sendMessage("Well, Tarnum?",group);
--- functions.js
+++ functions.js
@@ -4,8 +4,43 @@ var cleverbot = require('cleverbot.io');
 var jsonfile = require('jsonfile');
 var extend = require('util')._extend;
 
+//cleverbot stuff
+var bot = new cleverbot('TYHRwNcZFicTF4xI','rKarZL4vSevwLDnjLXnGK7MRkBwud1W1');
+bot.setNick("clayton");
+bot.create(function(err,Clayton){});
+
+// rps stuff
+var rpsCountdown;
+var gameInProgress = false; //stores whether there's an active rock-paper-scissors game
+var playerHand = ""; //player choice for rps
+var botHand = ""; //bot choice for rps
+
+var eightball = 
+		[
+		"It is certain",
+		"It is decidedly so",
+		"Without a doubt", 
+		"Yes, definitely",
+		"Yes, definitely",
+		"You may rely on it",
+		"As I see it, yes",
+		"Most likely",
+		"Outlook good",
+		"Yes",
+		"Signs point to yes",
+		"Reply hazy try again",
+		"Ask again later",
+		"Better not tell you now",
+		"Cannot predict now",
+		"Concentrate and ask again",
+		"Don't count on it",
+		"My reply is no",
+		"My sources say no",
+		"Outlook not so good",
+		"Very doubtful"];
+
 module.exports = {
-	kukup: function(api, group, deeb) {
+	kukUp: function(api, group, deeb) {
 		api.sendMessage("HO HO HO", group);
         api.changeNickname("Bhuge Dumbass", group, deeb);
 	},
@@ -17,7 +52,7 @@ module.exports = {
 		api.changeGroupImage(fs.createReadStream("tyson.jpg"), group, function callback(err) {
 	   		if(err) return console.error(err);
 	   	});
-		api.setTitle("THE MOST ASTOUNDING FACT", group, function(err,obj){});
+		api.setTitle("THE MOST ASTOUNDING FACT", group, function callback(err,obj){});
 	},
 	surendrekt: function(api, group, roon) {
 		api.sendMessage("Surendrekt", group);
@@ -27,6 +62,9 @@ module.exports = {
 	},
 	hegg: function(api, group) {
 		api.sendMessage({attachment: fs.createReadStream('hegg.gif')}, group);
+	},
+	ecksDee: function(api, group) {
+		api.sendMessage("😂🔫", group);
 	},
 	compliment: function(api, group, messageEvent) {
 		api.getUserInfo(messageEvent.senderID, function(err, ret) {
@@ -40,4 +78,71 @@ module.exports = {
 	    var randomUser = threadInfo.participantIDs[Math.floor(Math.random() * size)];
 	    api.sendMessage("Hoot! You must kill God.", randomUser);
 	},
+	eightball: function(api, group, messageEvent, deeb) {
+		if (messageEvent.senderID == deeb) 
+			api.sendMessage("Debarshi, you bitch", group);
+		else 
+			api.sendMessage(eightball[Math.floor(Math.random()*20)], group);		
+	},	
+	talk: function(api, group, input) {
+		bot.ask(input,function(err,response){
+			if (err) return console.error(err);
+				api.sendMessage(response,group);
+			});
+	},	
+	jankenPon: function(api, group, input) {
+		if (!gameInProgress) {	//makes sure there isn't an ongoing game before starting a new one	        						        					
+		    api.sendMessage("Saisho wa guu!", group);
+		    setTimeout(function() {
+		    	api.sendMessage("Janken pon!", group);
+		    }, 1500);
+		    gameInProgress = true; //flag to make sure new games can't start until this one is done
+		    botHand = ["✊","✋","✌"][Math.floor(Math.random()*3)]; //randomize bot choice	
+		    //start a countdown to end the game if the player doesn't make a choice	in time       							
+		    rpsCountdown = setTimeout(function() { 	        					
+			   	console.log("Game timed out"); 		        						        					
+			   	gameInProgress = false;
+			}, 10000);
+			return;
+		}
+		//set playerHand as per valid user input
+		if(input === '/rock' || input === "/guu" || input === "✊") 
+		    playerHand = "✊";       								        						        					
+		if(input === '/paper' || input === "/paa" || input === "✋") 
+		    playerHand = "✋";  			        								        						        					
+		if(input === '/scissors' || input === "/choki" || input === "✌") 
+		    playerHand = "✌";
+		//game logic		        								        								        						
+		if (playerHand === "✊" || playerHand === "✋" || playerHand === "✌" ) {
+		    var winner = "It's a tie!";
+		    if (playerHand === "✊") {
+		    	if (botHand === "✋")
+		    		winner = "bot";
+		    	if (botHand === "✌")
+		    		winner = "player";
+		    }
+		    if (playerHand === "✋") {		        							
+		    	if (botHand === "✌")
+		    		winner = "bot";
+		    	if (botHand === "✊")
+		    		winner = "player";
+		    }
+		    if (playerHand === "✌") {
+		    	if (botHand === "✊")
+		    		winner = "bot";
+		    	if (botHand === "✋")
+		    		winner = "player";
+		    }
+		    if(winner === "bot")
+		    	winner = "Looks like I win! Naisu."
+		    if(winner === "player")
+		    	winner = "Looks like you win! Naisu."
+		    clearTimeout(rpsCountdown); //cancel game timeout if game resolves successfully
+		    gameInProgress = false;	
+		    playerHand = "";	
+		    api.sendMessage(botHand, group, function() {
+		    	api.sendMessage(winner, group);	
+		    }); 						
+		}	
+	}
 }
