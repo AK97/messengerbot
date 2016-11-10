def466fe4953c40600df65a1e601964fe738a730
YordleLover97 <pxchoudhury@gmail.com>
2016-11-10 01:09:52 -0500

    Preemptive changes for impending merge

--- fbchatjs.js
+++ fbchatjs.js
@@ -18,14 +18,13 @@ login({email: "kenbhone@gmail.com", password: "naisubhig"}, function callback(er
     var aaron = '100003952090241'; //aaron's user ID
     var deeb = '1683495739'; //deeb's user ID	
 
-	var testChat = '1144974435591141'; // test chat
+	/*var testChat = '1144974435591141'; // test chat
     group = testChat;	
-	deeb = aaron;
+	deeb = aaron;*/
 
 	//data stuff
 	var trackingData = {};
 	var usageData = {};
-	var threadInfo = {};
 
 	// api.sendMessage("Enter B H I G", group);	
 
@@ -34,42 +33,75 @@ login({email: "kenbhone@gmail.com", password: "naisubhig"}, function callback(er
     	threadInfo = info;
 
 		try {
-			trackingData = jsonfile.readFileSync('tracking_data.json');
+			trackingData = jsonfile.readFileSync('json/tracking_data.json');
 		} catch(err) {
-	    	for (var x in threadInfo.participantIDs) {
-	    		var y = threadInfo.participantIDs[x];
+	    	for (var x in info.participantIDs) {
+	    		var y = info.participantIDs[x];
 	    		trackingData[y] = 0;
 	    	}
 		}
 
 		// make usage data stuff here
+		try {
+			usageData = jsonfile.readFileSync('json/usage_data.json');
+		}
+		catch(error) {}
 
 	    var stopListening = api.listen(function callback(err, event) {
 	        if (err) return console.error(err);
-	        if (event.threadID != group) {return;}
+	        if (event.threadID != group) 
+	        	return;
 	        var input = event.body;
+	        function caseUsage() {
+	        	if (usageData[input])
+	        		usageData[input]++;
+	        	else
+	        		usageData[input] = 1;
+	        }
+	        var cases = true;
         	if (event.type == "message" && input) {
     			switch(input) {
     				case '/stats':
         				api.getThreadInfo(group, function callback(err, info) {
         					if (err) return console.error(err);
-	        				threadInfo = info;
 				        	var output = extend({}, trackingData);
-				        	for (var x in threadInfo.nicknames) {
+				        	var sortedOutput = [];
+				        	var size = Object.size(tracking_data);
+				        	for (var i = 0; i < size; i++) {
+				        		var min = 1000000;
+				        		var minUser = "";
+				        		for (var x in output) {
+				        			if (output[x] < min) {
+				        				min = output[x];
+				        				minUser = x;
+				        			}
+					        	}
+					        	sortedOutput[i] = {minUser, min};
+				        	}
+
+				        	printedOutput = "Message sent:";
+ 
+ 				        	for (var x in sortedOutput) {
+ 				        		y = Object.keys(obj);
+ 				        		key = y[0];
+ 				        		printedOutput = printedOutput + key + ": " + x[key] + '/n';
+ 				        	} 				        	
+ 
+ 				        	for (var x in info.nicknames) {
 				        		if (x in output) {
-				        			output[threadInfo.nicknames[x]] = output[x];
+				        			output[info.nicknames[x]] = output[x];
 									delete output[x];
 				        		}
 				        	}
-				        	var sorted_output = Object.keys(output).sort(function(a,b){return output[a]-output[b]});
-				        	api.sendMessage(JSON.stringify(sorted_output, null, 4), group);	        					
+				        	var sortedOutput = Object.keys(output).sort(function(a,b){return output[a]-output[b]});
+				        	api.sendMessage(JSON.stringify(sortedOutput, null, 4), group);	        					
         				});
         				break;
         			case '/kukup':	        			
         				functions.kukUp(api, group, deeb);
         				break;
-        			case '/dab':
-        				functions.dab(api, group);
+        			case '/whip':
+        				functions.whip(api, group);
     					break;
     			    case '/gloriousdawn':
 	    				functions.gloriousDawn(api, group);
@@ -88,15 +120,14 @@ login({email: "kenbhone@gmail.com", password: "naisubhig"}, function callback(er
 	    				break;
 	    			case '/stopthemadness':
 	    				api.sendMessage("fuk the frik off", group);
-	    				var file = 'tracking_data.json'
-						 
-						jsonfile.writeFile(file, trackingData, function (err) {
-							if (err) console.error(err);
-						})
+	    				json.writeFile('json/tracking_data.json', trackingData, function callback (err)
+	    				{
+	    					if (err) return console.error(err);
+	    				});						 
 	    				return stopListening();
 	    				break;
 	    			case '/hoot':
-	    				functions.hoot(api, group, threadInfo)
+	    				functions.hoot(api, group, info)
 	    				break;       			
 		    		case '/rps': case '/jkp':
 		    			functions.jankenPon(api, group, event.senderID);			    				  					        				
@@ -106,19 +137,46 @@ login({email: "kenbhone@gmail.com", password: "naisubhig"}, function callback(er
 					case '/scissors': case "/choki": case "✌":			    	
 		    			functions.jankenPonParser(api, group, input);			    				  					        				
 		    			break;
-		    		default:  
-		    			if (input.indexOf("/8ball")==0 || input.indexOf("🎱")==0) 
-		            		functions.eightball(api, group, event, deeb)		    			
-		    			else if(input.indexOf("/talk")==0) 
-		    				functions.talk(api, group, input.replace("/talk ",""));						
-						else if(input.search("Clayyton") >= 0 || input.search("clayyton") >= 0) 
-							functions.talk(api, group, input);
+		    		default:
+		    			cases = false;  
+		    			if (input.indexOf("/8ball") == 0 || input.indexOf("🎱") == 0) {
+		            		functions.eightball(api, group, event, deeb);
+		            		if (usageData['/8ball']) 
+		            			usageData['/8ball']++; 
+		            		else 
+		            			usageData['/8ball'] = 1;	    			
+		    			}
+		    			else if (input.indexOf("/talk") == 0 || input.indexOf("/t") == 0) 
+		    			{
+		    				var talking = input.replace("/talk","");
+ 							talking = talking.replace("/t","");
+		    				functions.talk(api, group, talking);		    					
+		    				if (usageData['/talk'])
+		    					usageData['/talk']++; 
+		    				else 
+		    					usageData['/talk'] = 1;				
+		    			}
+						else if (input.search("Clayyton") >= 0 || input.search("clayyton") >= 0) 
+						{
+							var talking = input.replace("Clayyton","");
+ 							talking = talking.replace("clayyton","");
+							functions.talk(api, group, talking);
+							if (usageData['/talk']) 
+								usageData['/talk']++; 
+							else 
+								usageData['/talk'] = 1;
+						}
 	    		}	    
 
 	    		if (Math.random() > .99) 
-		        	functions.talk(api, group, input);		        
-				if (event.senderID) 
-					trackingData[event.senderID]++;		        	
+		        	functions.talk(api, group, input);	
+		        // data
+ 	    		if (cases) 
+ 	    			caseUsage(); 				        
+				if (event.senderID) {
+					trackingData[event.senderID]++;	
+					console.log(event);
+				}	        	
 		    }
 
 	        else if (event.type == "read_receipt"){
